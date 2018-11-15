f29ed2be2b80ed2ae32627af0a27b6d61949bbd3
Alexander Grigoriev <grigoriev@megaputer.ru>
2018-11-15 12:21:37 +0300

    Place 'build' files into repo
    Additional changes:
    - Update packages versions
    - Use yarn as a package manager

---
+++ build/sprite-generator.d.ts
@@ -0,0 +1,55 @@
+/**
+ * Limitations for icons:
+ *  - file name must not start with numbers.
+ */
+export interface Sprite {
+    name: string;
+    sourceFolder: string;
+    include?: RegExp;
+}
+export interface TargetFolder {
+    icons: string;
+    scss: string;
+    ts: string;
+}
+/**
+ * Used in output SCSS files: '.base.sprite{#}.icon{#}', '.base.size{#}'
+ */
+export interface CSSClasses {
+    base: string;
+    sprite: string;
+    size: string;
+    icon: string;
+}
+export interface Options {
+    sprites: Sprite[];
+    targetFolder: TargetFolder;
+    classes: CSSClasses;
+    /**
+     * Path in SCSS to sprite.
+     * Url must contain `#SPRITE_FILE`, which is replaced by sprite name with extension.
+     */
+    url: string;
+}
+export declare class SpriteGenerator {
+    private options;
+    private sizes;
+    private queue;
+    constructor(options: Options);
+    generate(): void;
+    private batchSvg;
+    private batchPng;
+    private done;
+    private deleteFolderRecursive;
+    private deleteTargetFolders;
+    private writeSprite;
+    private writeSCSS;
+    private writeSizesSCSS;
+    private writeTS;
+    private writeFile;
+    private getAlertComment;
+    private createFolderIfNotExist;
+    private getFilePaths;
+    private getClassName;
+    private getSpriteUrl;
+}
